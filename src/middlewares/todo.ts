a198c5b7795d6e0d3409be35d65c1285ed2254ce
tkmktkhr <mukku.wordpress@gmail.com>
2020-07-16 21:47:30 +0900

    Add todo model

--- src/middlewares/todo.ts
+++ src/middlewares/todo.ts
@@ -1,14 +1,10 @@
 import { RouterContext, Status } from "../deps.ts";
-
-// 絶対パスでないとだめ
-const FILE_PATH = "./src/db/todos.json";
+import { todoModel } from "../models/mod.ts";
 
 export async function getAll(ctx: RouterContext) {
-  const data = await Deno.readFile(FILE_PATH);
-  const decoder = new TextDecoder();
-  const todos = JSON.parse(decoder.decode(data));
+  const todos = await todoModel.getAll();
   ctx.response.status = Status.OK;
   ctx.response.body = {
-    todo: todos,
+    data: todos,
   };
 }
---
+++ src/models/todo.ts
@@ -0,0 +1,16 @@
+interface Todo {
+  id: string;
+  title: string;
+  done: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// 絶対パスでないとだめ
+const FILE_PATH = "./src/db/todos.json";
+
+export async function getAll(): Promise<Todo[]> {
+  const data = await Deno.readFile(FILE_PATH);
+  const decoder = new TextDecoder();
+  return JSON.parse(decoder.decode(data));
+}
