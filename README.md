60234ec3c5384cd503cd64ab3e92818350085f91
tkmktkhr <mukku.wordpress@gmail.com>
2020-07-27 22:07:42 +0900

    Update deno mosules

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # deno-project
 Sample Deno programme 
+
+## Deno run 
+### By denon
+denon start
+
+### By deno
+deno run --allow-net --allow-env --allow-read ./src/server.ts
+
+## Deno cache reload
+deno cache --reload src/deps.ts
+
+## Check a tree listing of all dependencies
+deno info --no-check src/deps.ts
+
+## Deno test
+### Single file
+deno test ./src/middlewares/__test__/root.test.ts
--- src/deps.ts
+++ src/deps.ts
@@ -5,13 +5,13 @@ export {
   Context,
   isHttpError,
   RouterContext,
-} from "https://deno.land/x/oak@v5.3.1/mod.ts";
+} from "https://deno.land/x/oak@v6.0.1/mod.ts";
 
 export {
   bold,
   yellow,
   green,
   cyan,
-} from "https://deno.land/std@0.60.0/fmt/colors.ts";
+} from "https://deno.land/std@0.62.0/fmt/colors.ts";
 
-export { v4 as uuid } from "https://deno.land/std@0.60.0/uuid/mod.ts";
+export { v4 as uuid } from "https://deno.land/std@0.62.0/uuid/mod.ts";
